5c1722ade9158b68bf50fe828683654f586beb93
EdimarioJr <edimariojunior.14@gmail.com>
2022-09-08 14:13:28 -0300

    init test npm

--- src/components/organisms/Table/TableCompound/TableHeaderRows.tsx
+++ src/components/organisms/Table/TableCompound/TableHeaderRows.tsx
@@ -1,29 +1,42 @@
 /* eslint-disable no-case-declarations */
-import { Tr, Th, chakra, TableRowProps, TableColumnHeaderProps } from '@chakra-ui/react';
-import { GoTriangleUp, GoTriangleDown } from 'react-icons/go';
+import {
+  Tr,
+  Th,
+  chakra,
+  TableRowProps,
+  TableColumnHeaderProps,
+} from "@chakra-ui/react";
+import { GoTriangleUp, GoTriangleDown } from "react-icons/go";
 
-import { useTableContext } from './TableContainer';
+import { useTableContext } from "./TableContainer";
 
 export type TableHeaderRowsProps = {
   headerRowProps?: TableRowProps;
   headerCellProps?: TableColumnHeaderProps;
 };
 
-export const TableHeaderRows = ({ headerRowProps, headerCellProps }: TableHeaderRowsProps) => {
+export const TableHeaderRows = ({
+  headerRowProps,
+  headerCellProps,
+}: TableHeaderRowsProps) => {
   const { headerGroups } = useTableContext();
 
   return (
     <>
       {headerGroups.map((headerGroup, indexGroup) => (
-        <Tr {...headerGroup.getHeaderGroupProps()} key={indexGroup} {...headerRowProps}>
+        <Tr
+          {...headerGroup.getHeaderGroupProps()}
+          key={indexGroup}
+          {...headerRowProps}
+        >
           {headerGroup.headers.map((column, indexHeader) => (
             <Th
               {...column.getHeaderProps(column.getSortByToggleProps())}
               key={indexHeader}
               {...headerCellProps}
             >
-              {column.render('Header')}
-              <chakra.span pl="4" d="inline-block">
+              {column.render("Header")}
+              <chakra.span pl="4" display="inline-block">
                 {column.isSorted ? (
                   column.isSortedDesc ? (
                     <GoTriangleUp aria-label="sorted descending" />
